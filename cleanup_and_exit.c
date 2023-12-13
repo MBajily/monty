cbf3d93042daa12af34a217c75164c0b0970dccd
Mohammed Elgaily <MBajily@gmail.com>
2023-12-13 18:30:00 +0300

    Betty styling.

--- cleanup_and_exit.c
+++ cleanup_and_exit.c
@@ -2,13 +2,17 @@
 
 
 /**
- * cleanup_and_exit - Performs cleanup tasks and exits the program with a failure status.
+ * cleanup_and_exit - Performs cleanup tasks and exits
+ * the program with a failure status.
+ *
  * @head: Pointer to the head of the stack.
  *
  * This function is responsible for performing necessary cleanup tasks before
- * exiting the program in case of an error. It closes the file, frees the memory
+ * exiting the program in case of an error.
+ * It closes the file, frees the memory
  * allocated for the content, and cleans up the stack by freeing all nodes.
- * It is intended to be called when an error occurs and the program needs to exit
+ * It is intended to be called when an error
+ * occurs and the program needs to exit
  * gracefully.
  *
  * @head: Pointer to the head of the stack.
