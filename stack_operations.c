8636b7cf0b27143ec2d984a879fb8f1ecf2e5de9
root <root@DESKTOP-PDMJJRJ.localdomain>
2023-12-14 01:28:49 +0100

    move these files (f_pull, f_push, fpint) to stack_operations file

--- stack_operations.c
+++ stack_operations.c
@@ -53,3 +53,82 @@ void f_swap(stack_t **head, unsigned int counter)
 	hd->n = hd->next->n;
 	hd->next->n = AX;
 }
+
+
+/**
+ * f_push - add new node to stack
+ *
+ * @head: head of stack
+ * @counter: line number
+ *
+ * Return: None
+ */
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, i = 0, f = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			i++;
+		for (; bus.arg[i] != '\0'; i++)
+			if (bus.arg[i] > '9' || bus.arg[i] < '0')
+				f = 1;
+		if (f == 1)
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		cleanup_and_exit(head);
+	}
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		add_node(head, n);
+	else
+		add_queue(head, n);
+}
+
+
+/**
+ * f_pall - prints the current stack
+ *
+ * @head: the head of stack
+ * @counter: null
+ *
+ * Return: none
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
+
+
+/**
+ * f_pint - prints the top
+ *
+ * @head: the head of the stack
+ * @counter: line number
+ *
+ * Return: none
+ */
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		cleanup_and_exit(head);
+	}
+	printf("%d\n", (*head)->n);
+}
+
